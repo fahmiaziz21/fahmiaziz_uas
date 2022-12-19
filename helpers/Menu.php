9f548557dd675bc109859c9074be188f4eab616d
fahmiaziz21 <120867672+fahmiaziz21@users.noreply.github.com>
2022-12-19 16:36:34 +0700

    first commite

---
+++ helpers/Menu.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Beranda', 
+			'icon' => '<i class="fa fa-home "></i>'
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Pekerjaan yang Gagal', 
+			'icon' => '<i class="fa fa-male "></i>'
+		),
+		
+		array(
+			'path' => 'ingredients', 
+			'label' => 'Komponen', 
+			'icon' => '<i class="fa fa-puzzle-piece "></i>'
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrasi', 
+			'icon' => '<i class="fa fa-random "></i>'
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Pengaturan Ulang Kata Sandi', 
+			'icon' => '<i class="fa fa-key "></i>'
+		),
+		
+		array(
+			'path' => 'preparation_steps', 
+			'label' => 'Langkah Persiapan', 
+			'icon' => '<i class="fa fa-steam-square "></i>'
+		),
+		
+		array(
+			'path' => 'recipe_line_items', 
+			'label' => 'Item Baris Resep', 
+			'icon' => '<i class="fa fa-sitemap "></i>'
+		),
+		
+		array(
+			'path' => 'recipes', 
+			'label' => 'Resep', 
+			'icon' => '<i class="fa fa-lemon-o "></i>'
+		),
+		
+		array(
+			'path' => 'units', 
+			'label' => 'Unit', 
+			'icon' => '<i class="fa fa-lightbulb-o "></i>'
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Pengguna', 
+			'icon' => '<i class="fa fa-github-alt "></i>'
+		)
+	);
+		
+	
+	
+}
